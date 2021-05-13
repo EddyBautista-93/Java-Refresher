21cae6528797b3155862b0228a0873052a59283f
EddyBautista <bautista.j.eddy@gmail.com>
2021-05-13 17:24:48 -0500

    add String2Bool

---
+++ src/String2Bool.java
@@ -0,0 +1,11 @@
+public class String2Bool {
+
+    public static String convert(boolean b){
+        return Boolean.toString(b);
+    }
+
+    public static void main(String[] args) {
+        convert(true);
+    }
+
+}
